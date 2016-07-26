93ea097458eda02bcac8aee61e1a68553e201040
verien <lu_fangru@hotmail.com>
2016-07-26 11:13:14 +0800

    Chapter 1

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+
+  def index
+    # flash[:notice] = "Good morning!!" #green
+    # flash[:alert] = "Good night!!"    #red
+    # flash[:warning] = "Bad boy!!"     #yellow
+  end
+end
