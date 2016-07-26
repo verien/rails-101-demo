3499ace54cb54cf48a1606e54c14a6df16637a8a
verien <lu_fangru@hotmail.com>
2016-07-26 15:21:12 +0800

    Chapter 3

---
+++ app/models/group.rb
@@ -0,0 +1,5 @@
+class Group < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :title, format: { with: /\A[a-zA-Z]+\z/, message: "Only allows letters"}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :groups
+
   # root 'Controller#Action'
   root 'welcome#index'
   # welcome#index表示，首頁會使用welcome這個控制器的index行為。
